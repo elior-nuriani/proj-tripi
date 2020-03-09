ae13c38ca25c180e562b6ea6c7fd31022d0d43f0
Elior Nuri <eliornuri777@gmail.com>
2020-03-09 21:23:49 +0200

    Fixing Bugs

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,5 +1,5 @@
-const {PASSWORD_ATLAS , USERNAME_ATLAS } = require('./details-helper')
+// const {PASSWORD_ATLAS , USERNAME_ATLAS } = require('./details-helper')
 
 module.exports = {
-  "dbURL":`mongodb+srv://${USERNAME_ATLAS}:${PASSWORD_ATLAS}@cluster0-52zqr.mongodb.net/test?retryWrites=true&w=majority`
+  "dbURL":`${process.env.MONGO_URL}`
 }
---
+++ backend/public/precache-manifest.a6c3e4252e9aba3244fc7d476e6bb2cf.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e95544362f953e43bd0",
+    "url": "/css/app.97b7166e.css"
+  },
+  {
+    "revision": "ee59d01f14af5e081a4f",
+    "url": "/css/chunk-vendors.6744baa1.css"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/fonts/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/fonts/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/fonts/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/fonts/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/fonts/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/fonts/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/fonts/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/fonts/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/fonts/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/fonts/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/fonts/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/fonts/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/fonts/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/fonts/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/fonts/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/fonts/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/fonts/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/fonts/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/fonts/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/img/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/img/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/img/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "d5bbdd0cad62bd1bdb711b5e730a10d7",
+    "url": "/img/img/dinner at the hill.jpg"
+  },
+  {
+    "revision": "ae4ab612353470581e64f9c88b89cb7f",
+    "url": "/img/img/inka.jpg"
+  },
+  {
+    "revision": "7064d5002694dc99b93f9274925bbf89",
+    "url": "/img/img/jordan.jpg"
+  },
+  {
+    "revision": "3a4d271caa48b17b799c8ccb6d993d92",
+    "url": "/img/img/safari.jpg"
+  },
+  {
+    "revision": "e62baa4b72175e2345103a9b62fde6ff",
+    "url": "/img/img/zurich village.jpg"
+  },
+  {
+    "revision": "132be427ccb39f50fce7bd6b70625ba5",
+    "url": "/img/view-hero.132be427.jpg"
+  },
+  {
+    "revision": "799835f7c18f110fad8922dcaab3de7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e95544362f953e43bd0",
+    "url": "/js/app.c6170d94.js"
+  },
+  {
+    "revision": "ee59d01f14af5e081a4f",
+    "url": "/js/chunk-vendors.ccc0a11d.js"
+  },
+  {
+    "revision": "cf2c20edcff3351ac3207a704dc0ad06",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
